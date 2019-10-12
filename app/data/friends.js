0179965711850354eb73d95506a7b6bd6c9538eb
Sultan Karimov <skarimovkgz@gmail.com>
2019-10-11 23:31:38 -0500

    final submition

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+	{
+		name: "Ziad",
+		photo: "https://data.whicdn.com/images/187773064/original.gif",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Zaya",
+		photo: "https://i1.wp.com/media2.giphy.com/media/WMpyrEnjfl4Gs/giphy.gif",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Ahmed",
+		photo: "https://media2.giphy.com/media/qdDO6hTcyeDks/giphy.gif",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Saikal",
+		photo: "https://media.tenor.com/images/1daebcd7ce0af1ff469962a4eca5b06b/tenor.gif",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Joe",
+		photo: "https://media0.giphy.com/media/mVJ5xyiYkC3Vm/giphy.gif",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+		name: "Brian",
+		photo: "https://data.whicdn.com/images/43800336/original.gif",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
